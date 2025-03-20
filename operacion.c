fe7d2d986a3566b67b84d9769518e07b256771d7
Lobo-of-War <a01255264@tec.mx>
2025-03-19 17:46:16 -0700

    Codigo depurado

--- operacion.c
+++ operacion.c
@@ -5,9 +5,13 @@
 int main(){
     int num1 = 12;
     int num2 = 3;
-    int num3 = 1
-    int suma(a, b){
-        return a+b
-    }
+    
+    int suma(int a, int b){
+        return a+b;
+     }
 
+    int resta(int a, int b){
+    return a-b;
+    }
+   
 }
